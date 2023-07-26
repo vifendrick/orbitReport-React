5b1c3d1cb92656fc6d8ec68fe7baae2015e13ae4
Vi Fendrick <violetynaf@gmail.com>
2023-07-25 20:41:36 -0500

    done

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,19 @@
-const Buttons = () => {
+import satData from "./satData";
+
+const Buttons = (props) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {props.displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => props.filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+      })}
+      <button onClick={() => { props.setSat(satData)}}>All Orbits</button>
+    </div>
   );
 };
 
 export default Buttons;
+
